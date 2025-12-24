3b107ab3128ef031944dd8a7f8f9f14acff5b80e
Jugal pakhare <jugalpkhr@gmail.com>
2025-12-24 22:07:57 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+// login.js
+import { auth } from "./firebase.js";
+import {
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("click", async () => {
+  try {
+    await signInWithPopup(auth, provider);
+    // ✅ login is complete immediately
+    window.location.replace("ikndex.html");
+  } catch (err) {
+    console.error("Login failed:", err);
+  }
+});
